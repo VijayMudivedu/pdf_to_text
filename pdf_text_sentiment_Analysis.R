46fe5f373276d19227d32746fd7b15fc1fd8d429
Vijay Mudivedu <Vijay.Mudivedu@iiitb.net>
2018-06-11 19:11:56 +0530

    prettying the code

--- pdf_text_sentiment_Analysis.R
+++ pdf_text_sentiment_Analysis.R
@@ -11,9 +11,15 @@ library(pdftools)
 library(tm)
 library(googledrive)
 
-setwd("/Users/vmudivedu/OneDrive/OneDrive - Atimi Software Inc/Upgrad/case study/pdf_to_text")
+setwd(
+  "/Users/vmudivedu/OneDrive/OneDrive - Atimi Software Inc/Upgrad/case study/pdf_to_text"
+)
 # download the data from google drive
-drive_download(as_dribble(as_id("https://drive.google.com/file/d/1gZCnlhwVMBIE0SugUUxDIgQrfVz-cDQR/")),overwrite = TRUE)
+drive_download(as_dribble(
+  as_id(
+    "https://drive.google.com/file/d/1gZCnlhwVMBIE0SugUUxDIgQrfVz-cDQR/"
+  )
+), overwrite = TRUE)
 # store the data in the
 java_basic_pdf_to_text <-  pdf_text(pdf = "JavaBasics-notes.pdf")
 View(java_basic_pdf_to_text)
@@ -24,23 +30,14 @@ str(java_basic_pdf_to_text)
 #----------------------------
 
 # remove the "Java Basics" header
-cleaned_java_basics <- str_replace_all(string = java_basic_pdf_to_text,pattern = "Java Basics\n|\\\n|\\•|[[:]]|© 1996-2003 jGuru.com. All Rights Reserved.|Java Basics -",replacement = "")
+cleaned_java_basics <-
+  str_replace_all(string = java_basic_pdf_to_text,
+                  pattern = "Java Basics\n|\\\n|\\•|[[:]]|© 1996-2003 jGuru.com. All Rights Reserved.|Java Basics -",
+                  replacement = "")
 
-# java_basic_text <- str_split(string = java_basic_pdf_to_text,pattern = "Java Basics\n",simplify = T)
-# View(java_basic_text)
-# # convert the
-# java_basic_text <- as.data.frame(java_basic_text,stringsAsFactors = F)
-# java_basic_text <- java_basic_text[,-1]
-# 
-# str(java_basic_text)
-# 
-# # copy the data
-# java_basic_text$V2[1] <- java_basic_text$V3[1]
-# java_basic_text <- java_basic_text[,-2]
-# 
 
 # combining all the text from different pages.
-reviewed_text <- paste(cleaned_java_basics,collapse = "")
+reviewed_text <- paste(cleaned_java_basics, collapse = "")
 reviewed_text
 
 #
@@ -54,19 +51,21 @@ crps_java_basics
 # Further Cleaning of text
 
 # Lower case
-crps_cleaned <- tm_map(crps_java_basics,content_transformer(tolower))
+crps_cleaned <-
+  tm_map(crps_java_basics, content_transformer(tolower))
 
 # removing punctuations
-crps_cleaned <- tm_map(crps_cleaned,removePunctuation)
+crps_cleaned <- tm_map(crps_cleaned, removePunctuation)
 
 # removing white spaces
-crps_cleaned <- tm_map(crps_cleaned,stripWhitespace)
+crps_cleaned <- tm_map(crps_cleaned, stripWhitespace)
 
 # removing numbers
-crps_cleaned <- tm_map(crps_cleaned,removeNumbers)
+crps_cleaned <- tm_map(crps_cleaned, removeNumbers)
 
 # removing stopwords
-crps_cleaned <- tm_map(crps_cleaned,removeWords,stopwords("english"))
+crps_cleaned <-
+  tm_map(crps_cleaned, removeWords, stopwords("english"))
 
 
 # document term-matirx for the cleaned text
@@ -75,21 +74,31 @@ dtm2 <- as.matrix(dtm)
 head(dtm2)
 
 # Most frequent terms are:
-ft_java_basics <- findFreqTerms(x = dtm,lowfreq = 10,highfreq = Inf)
+ft_java_basics <- findFreqTerms(x = dtm,
+                                lowfreq = 10,
+                                highfreq = Inf)
 ft_java_basics
 
 # frequency of the most frequent terms
 freq_java_basic <- colSums(dtm2)
-freq_java_basic <- sort(freq_java_basic,decreasing = TRUE)
+freq_java_basic <- sort(freq_java_basic, decreasing = TRUE)
 
-head(freq_java_basic,n = 50)
+head(freq_java_basic, n = 50)
 # creating the word dataframe
-word_df <- data.frame(words = names(freq_java_basic),freq = as.vector(freq_java_basic))
+word_df <-
+  data.frame(words = names(freq_java_basic),
+             freq = as.vector(freq_java_basic))
 
 # Printing the Keywords In Java_basic in the text file.
 library(RColorBrewer)
 set.seed(100)
-wordcloud(words = word_df$words,freq = word_df$freq,
-          min.freq = 5,random.order = F,max.words = 200,
-          rot.per = 0.35,colors = brewer.pal(n = 8,name = "Dark2"),
-          random.color = F)
+wordcloud(
+  words = word_df$words,
+  freq = word_df$freq,
+  min.freq = 5,
+  random.order = F,
+  max.words = 200,
+  rot.per = 0.35,
+  colors = brewer.pal(n = 8, name = "Dark2"),
+  random.color = F
+)
